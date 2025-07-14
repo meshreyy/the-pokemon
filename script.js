e3db811ef95b20ddea3e5967ee151f67ebcbf1ae
meshreyy <iamjhashreya@gmail.com>
2025-07-14 19:50:56 +0530

    updated script file

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+async function fetchData() {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/type/fire');
+        response.id='count';
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+
+
+function sCategory() {
+    const inputEl = document.getElementById('category');
+    inputEl.id='category';
+    const value = inputEl.value;
+    console.log(value);
+
+
+}
+
 
